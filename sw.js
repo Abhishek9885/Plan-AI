f0e55f39ad3e4f6d82ce09e350f9da6617e965ec
Abhishek Singh <singhabhishek4964@gmail.com>
2026-04-01 18:44:54 +0530

    fix: bump service worker to forcefully purge old caches on clients

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service worker disabled - v4
+// Service worker disabled - v5
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
